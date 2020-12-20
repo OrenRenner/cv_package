e0545c4439ff3c6b4d431b8210021a1ad8c178a1
OrenRenner <47448028+OrenRenner@users.noreply.github.com>
2020-12-20 16:24:01 +0500

    Update im_list.py

--- cv_pack/im_list.py
+++ cv_pack/im_list.py
@@ -2,8 +2,52 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-#!pip install opencv-python==3.4.2.16
-#!pip install opencv-contrib-python==3.4.2.16
+def compute_harris_response(im, sigma=3):
+    im = rgb2gray(im)
+    imx = np.zeros(im.shape)
+    imy = np.zeros(im.shape)
+    filters.gaussian_filter(im, (sigma, sigma), (0, 1), imx)
+    filters.gaussian_filter(im, (sigma, sigma), (1, 0), imy)
+
+    Wxx = filters.gaussian_filter(imx*imx, sigma)
+    Wxy = filters.gaussian_filter(imx*imy, sigma)
+    Wyy = filters.gaussian_filter(imy*imy, sigma)
+
+    Wdet = Wxx*Wyy - Wxy**2
+    Wtr = Wxx + Wyy
+
+    return Wdet/Wtr
+
+def get_harris_points(harrisim, min_dist=10, threshold=0.1):
+    corner_threshold = harrisim.max() * threshold
+    harrisim_t = (harrisim > corner_threshold) * 1
+    coords = np.array(harrisim_t.nonzero()).T
+
+    candidate_values = [harrisim[c[0], c[1]] for c in coords]
+    index = np.argsort(candidate_values)
+
+    allowed_locations = np.zeros(harrisim.shape)
+    allowed_locations[min_dist:-min_dist, min_dist:-min_dist] = 1
+
+    filtered_coords = []
+    for i in index:
+        if allowed_locations[coords[i,0], coords[i,1]] == 1:
+            filtered_coords.append(coords[i])
+            allowed_locations[(coords[i,0]-min_dist):(coords[i,0]+min_dist),
+                              (coords[i,1]-min_dist):(coords[i,1]+min_dist)] = 0
+    
+    return filtered_coords
+
+def get_descriptors(image, filtered_coords, wid=5):
+    desc = []
+    for coords in filtered_coords:
+        patch = image[
+                      coords[0] - wid : coords[0] + wid + 1,
+                      coords[1] - wid : coords[1] + wid + 1
+        ].flatten()
+        desc.append(patch)
+    
+    return desc
 
 def matching_SURF(train_image_path, test_image_path):
     #train_image_path, test_image_path - путь к тренировочному и тестовому изображениям для сопоставления
